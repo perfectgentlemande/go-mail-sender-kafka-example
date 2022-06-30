165323727d82847716c066e87579feea66618fea
Артем Чаадаев <perfectgentleman.de@ya.ru>
2022-07-01 02:26:48 +0300

    removed unnecessary print

--- internal/messagebroker/messagebroker.go
+++ internal/messagebroker/messagebroker.go
@@ -37,8 +37,6 @@ func (b *Broker) ReadLetter(ctx context.Context) (service.Letter, error) {
 		return service.Letter{}, fmt.Errorf("cannot read message: %w", err)
 	}
 
-	fmt.Printf("message at topic/partition/offset %v/%v/%v: %s = %s\n", m.Topic, m.Partition, m.Offset, string(m.Key), string(m.Value))
-
 	lt := service.Letter{}
 	err = json.Unmarshal(m.Value, &lt)
 	if err != nil {
